d557226e284fefee34491937d355667e53d1477c
piicollo <kapilchharahangrai@gmail.com>
2025-08-24 08:53:42 +0545

    second commit 2025

---
+++ first.js
@@ -0,0 +1,190 @@
+// let num1 = prompt("Enter first number: ");
+// let num2 = prompt("Enter Second number: ");
+
+// num1 = parseFloat(num1);
+// num2 = parseFloat(num2);
+
+// let sum = num1 + num2;
+
+// console.log("The sum of " + num1 + " + " + num2 + " = " + sum);
+// console.log("Hello World!"); //printing output
+
+//var declaration 
+// var age = 23;
+//updating value of age
+
+// age = 30;
+// Printing output using console
+// console.log(age);
+
+// let declaration
+
+// let name = "Hello JS";
+
+// name = "MERN"
+// console.log(name);
+
+// Declaration of const
+
+// const product = "Pantafast";
+// console.log(product);
+
+// const pi = 3.14159;
+// const user = { name: "Kapil" };
+// console.log(pi);
+
+// let a = 5;
+// let b = 3;
+
+// const c = a % b;
+// console.log(c);
+
+// Addition Assignment Operator
+// let a = 2;
+// a += 5;
+// console.log(a);
+
+//Multiply  Assignment Operator
+// let a = 2;
+// a = a * 3;
+// console.log(a);
+
+// Division Assignment Operator
+// let a = 6;
+// a = a / 2;
+// console.log(a);
+
+//Remainder or Modulus Assignment Operator
+
+// let a = 7;
+// a %= 2;
+// console.log(a);
+
+//Subtract Assignment Operator
+
+// let a = 4;
+// a -= 5;
+// console.log(a);
+
+//LOGICAL OPERATOR
+
+// And && Logical Operator
+
+// let a = true;
+// let b = false;
+// console.log(a && b);
+
+// Array
+// const arr = [1, 2, 3, 4, 5];
+// arr[0] = 1000;
+// console.log(arr)
+
+// Object in JS
+
+// const person = {
+//     firstName : "Kapil",
+//     lastName : "Rai",
+//     age : 28
+// }
+
+// // console.log(person.age)
+// console.log(person.firstName); 
+// console.log(person["age"]);
+// console.log(person["lastName"]);
+
+// Function in JS
+// Named Function 
+
+// function helloWorld(name) {
+//     console.log("Hello " + name);
+// }
+// helloWorld("Kapil");
+
+//Function With Parameters
+
+// function greet(firstName, lastName) {
+//     console.log("Hello " + firstName + " " + lastName);
+// }
+// greet("Kapil", "Rai");
+
+// Normal function
+
+// function sum() {
+//     console.log(2 + 3);
+// }
+// sum()
+
+// function name(firstName, lastName) {
+//     console.log("Welcome abroad Mr. " + firstName + " " + lastName + ".")
+// }
+
+// function sum() {
+//     console.log(3 + 2);
+
+// }
+// sum()
+
+// Anonymous Function
+
+// const y = function() {
+//     return 2 + 3;
+// }
+// let z = y();
+// console.log(z);
+
+// Printing "Hello" world using function in js
+
+// function greet() {
+//     console.log('H');
+//     console.log('E');
+//     console.log('L');
+//     console.log('L');
+//     console.log('0');
+// }
+// greet();
+
+// Normal function
+// function addTwoNumbers(num1, num2) {
+//     console.log(num1 + num2);
+// }
+
+// const result = addTwoNumbers(3, 2);
+
+// console.log("Result: ", result);
+
+// String function 
+
+// const person = { name: "Harry" };
+// person.name = "John"; 
+// person = { name: "New" };
+// // console.log(person);
+
+// const myObject = {
+//   name: "Alice",
+//   age: 30,
+//   city: "New York"
+// };
+
+// console.log(myObject);
+// console.log(myObject.age);
+
+// document.getElementById("dom").innerHTML = "DOM Tree"
+
+// Declaring variables in JS 
+
+// let message = "Hello";
+// console.log(message);
+
+// var _message = "World";
+// console.log(_message);
+
+// const $num = 3;
+// console.log($num)
+
+// var name = "Kapil";
+// var name = "Rai"; // ✅ Redeclaration allowed
+// console.log(name);
+
+const firstName = "Kapil";
+const lastName = "Rai";
+console.log(firstName + " " +lastName);
